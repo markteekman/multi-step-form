83afe9d166e4b4ee401589e57c8b8602b4e3ce27
Mark Teekman <markteekman@users.noreply.github.com>
2024-02-27 08:03:19 +0100

    Value logic for RadioButtonCard

--- src/components/RadioButtonCard.vue
+++ src/components/RadioButtonCard.vue
@@ -1,55 +1,66 @@
 <script setup>
+import { computed } from 'vue'
+import { useFormStore } from '../stores/form'
+const store = useFormStore()
+
 const props = defineProps({
-  bonus: {
-    type: String,
-    required: true,
-    default: '2 months free',
-  },
-  checked: {
-    type: Boolean,
-    required: false,
-    default: false,
-  },
-  description: {
-    type: String,
-    required: true,
-    default: 'Advanced for $12 a month',
-  },
   icon: {
     type: String,
     required: true,
-    default: 'advanced',
+    default: 'arcade',
   },
   id: {
     type: String,
     required: true,
-    default: 'advanced',
+    default: 'arcade',
+  },
+  monthlyPrice: {
+    type: String,
+    required: true,
+    default: '$9/mo',
   },
   name: {
     type: String,
     required: true,
     default: 'plan',
   },
-  price: {
-    type: String,
-    required: true,
-    default: '$9/mo',
-  },
-  showBonus: {
-    type: Boolean,
-    required: true,
-    default: false,
-  },
   title: {
     type: String,
     required: true,
-    default: 'Advanced',
+    default: 'Arcade',
   },
   value: {
     type: String,
     required: true,
-    default: 'advanced',
+    default: 'arcade',
   },
+  yearlyPrice: {
+    type: String,
+    required: true,
+    default: '$90/yr',
+  },
+})
+
+const emit = defineEmits(['update:modelValue'])
+
+function updateValue(event) {
+  store.updateFormData({ plan: event.target.value })
+}
+
+const currentPrice = computed(() => {
+  return store.billingCycle === 'monthly' ? props.monthlyPrice : props.yearlyPrice
+})
+
+const dynamicDescription = computed(() => {
+  if (store.billingCycle === 'monthly') {
+    return `${props.title} for ${props.monthlyPrice}`
+  } else {
+    return `${props.title} for ${props.yearlyPrice}, with 2 months free`
+  }
+})
+
+const showBonus = computed(() => {
+  return store.billingCycle === 'yearly'
 })
 </script>
 
@@ -61,24 +72,29 @@ const props = defineProps({
     <input
       type="radio"
       :id="id"
-      :value="value"
+      :value="id"
       :name="name"
-      :checked="checked"
+      :checked="store.formData.plan === value"
+      @change="updateValue"
     />
-    <span class="sr-only">{{ description }}</span>
-    <span class="radio-card__details">
+    <span class="sr-only">{{ dynamicDescription }}</span>
+    <span
+      class="radio-card__details"
+      aria-hidden="true"
+    >
       <img
-        :src="`/multi-step-form/icon-${icon}.svg`"
+        :src="`/multi-step-form/icon-${id}.svg`"
         alt=""
       />
       <span class="radio-card__content">
         <span class="content__title">{{ title }}</span>
-        <span class="content__price">{{ price }}</span>
+        <span class="content__price">{{ currentPrice }}</span>
         <span
           class="content__bonus"
           v-if="showBonus"
-          >{{ bonus }}</span
         >
+          2 months free
+        </span>
       </span>
     </span>
   </label>
@@ -92,7 +108,7 @@ const props = defineProps({
   display: block;
   flex: 1 1 0;
   inline-size: 0;
-  block-size: 160px;
+  block-size: 185px;
   padding: var(--space-s);
   border: 2px solid var(--neutral-cool-gray);
   border-radius: var(--radius-m);
@@ -128,15 +144,21 @@ const props = defineProps({
 .content {
   &__title,
   &__bonus {
-    color: var(--primary-marine-blue);
+    font-weight: bold;
   }
 
   &__title {
-    font-weight: bold;
+    color: var(--primary-marine-blue);
   }
 
   &__price {
     color: var(--neutral-text-gray);
+  }
+
+  &__bonus {
+    margin-block-start: var(--space-3xs);
+    color: var(--primary-purplish-blue);
+    font-size: var(--font-size--2);
   }
 }
 
